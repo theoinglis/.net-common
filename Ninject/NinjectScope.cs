e78ff823635b5049ec8a3ed31de9ab681c12c33d
Theo Inglis <theo.inglis@gmail.com>
2014-04-04 17:55:59 +0100

    Re add permanent files

---
+++ Ninject/NinjectScope.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Dependencies;
+using Ninject.Activation;
+using Ninject.Parameters;
+using Ninject.Syntax;
+
+namespace Common.Ninject
+{
+    public class NinjectScope : IDependencyScope
+    {
+        protected IResolutionRoot ResolutionRoot;
+
+        public NinjectScope(IResolutionRoot kernel)
+        {
+            ResolutionRoot = kernel;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            IRequest request = ResolutionRoot.CreateRequest(serviceType, null, new Parameter[0], true, true);
+            return ResolutionRoot.Resolve(request).SingleOrDefault();
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            IRequest request = ResolutionRoot.CreateRequest(serviceType, null, new Parameter[0], true, true);
+            return ResolutionRoot.Resolve(request).ToList();
+        }
+
+        public void Dispose()
+        {
+            var disposable = ResolutionRoot as IDisposable;
+            if (disposable != null) disposable.Dispose();
+            ResolutionRoot = null;
+        }
+    }
+}
