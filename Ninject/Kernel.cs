27abf87d2fa88e12370a7fa0f6983656abd6d3ba
Theo Inglis <theo.inglis@gmail.com>
2014-04-04 17:34:29 +0100

    First commit

---
+++ Ninject/Kernel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject;
+
+namespace Common.Ninject
+{
+    public static class Kernel
+    {
+        private static KernelBase _instance;
+        public static KernelBase Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    throw new Exception("Ninject Kernel Instance has not been set");
+                }
+                return _instance;
+            }
+            set
+            {
+                if (_instance != null)
+                {
+                    throw new Exception("Ninject Kernel Instance has already been set");
+                }
+                _instance = value;
+            }
+        }
+    }
+}
